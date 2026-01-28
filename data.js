dc708b9fd8a2a4c9e19dea63b094c57d82597518
AntoniGromek <01198007@pw.edu.pl>
2026-01-28 17:28:44 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+var daneAED = {
+    "type": "FeatureCollection",
+    "features": [
+        { "type": "Feature", "properties": { "miejsce": "Urząd Gminy Lesznowola", "dostep": "Godziny pracy urzędu" }, "geometry": { "type": "Point", "coordinates": [20.936657, 52.090698] } },
+        { "type": "Feature", "properties": { "miejsce": "Auchan Piaseczno", "dostep": "Godziny otwarcia" }, "geometry": { "type": "Point", "coordinates": [21.022879, 52.095725] } },
+        { "type": "Feature", "properties": { "miejsce": "Designer Outlet", "dostep": "Godziny otwarcia" }, "geometry": { "type": "Point", "coordinates": [21.023289, 52.086909] } },
+        { "type": "Feature", "properties": { "miejsce": "Decathlon Piaseczno", "dostep": "Godziny otwarcia" }, "geometry": { "type": "Point", "coordinates": [21.017012, 52.088126] } },
+        { "type": "Feature", "properties": { "miejsce": "OSP Józefosław", "adres": "ul. Urocza 14", "dostep": "Całodobowo" }, "geometry": { "type": "Point", "coordinates": [21.051225, 52.099165] } },
+        { "type": "Feature", "properties": { "miejsce": "Szkoła nr 5", "dostep": "Godziny pracy szkoły" }, "geometry": { "type": "Point", "coordinates": [21.021051, 52.077165] } },
+        { "type": "Feature", "properties": { "miejsce": "Basen GOSiR Piaseczno", "dostep": "6:00 - 22:00" }, "geometry": { "type": "Point", "coordinates": [21.018760, 52.079643] } },
+        { "type": "Feature", "properties": { "miejsce": "IV LO w Piasecznie", "dostep": "Godziny lekcyjne" }, "geometry": { "type": "Point", "coordinates": [21.032602, 52.076705] } },
+        { "type": "Feature", "properties": { "miejsce": "Starostwo Powiatowe", "dostep": "Godziny pracy" }, "geometry": { "type": "Point", "coordinates": [21.030892, 52.075872] } },
+        { "type": "Feature", "properties": { "miejsce": "Urząd Miasta i Gminy", "dostep": "Godziny pracy" }, "geometry": { "type": "Point", "coordinates": [21.027769, 52.073713] } },
+        { "type": "Feature", "properties": { "miejsce": "Komenda Policji", "dostep": "Całodobowo" }, "geometry": { "type": "Point", "coordinates": [21.026343, 52.073910] } },
+        { "type": "Feature", "properties": { "miejsce": "Straż Miejska", "dostep": "Całodobowo" }, "geometry": { "type": "Point", "coordinates": [21.025237, 52.071693] } },
+        { "type": "Feature", "properties": { "miejsce": "Stadion Miejski", "dostep": "W czasie wydarzeń" }, "geometry": { "type": "Point", "coordinates": [21.035788, 52.061320] } },
+        { "type": "Feature", "properties": { "miejsce": "Dworzec PKP Piaseczno", "dostep": "Całodobowo" }, "geometry": { "type": "Point", "coordinates": [21.017390, 52.067066] } }
+    ]
+};
